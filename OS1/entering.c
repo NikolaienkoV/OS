181bdf4941fb91825cb5fe90f2a4aab35b24f0ec
NikolaienkoV <100066427+NikolaienkoV@users.noreply.github.com>
2022-12-08 12:28:03 +0200

    Add files via upload

---
+++ OS1/entering.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+float entering_x(void)
+{
+  float temp_x = 0.0;
+  printf("\n\tEnter x: ");
+  scanf("%f", &temp_x);
+  return temp_x;
+}
---
+++ OS1/fx.c
@@ -0,0 +1,6 @@
+#include <math.h>
+
+float counting_fx(float x)
+{
+  return (exp(-fabs(x))*sin(x));
+}
